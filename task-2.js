2ceaf4e2a7ed022bc49e7820adb49a227615a362
anaybhadury <bhadury9292@gmail.com>
2024-09-04 21:26:16 +0600

    added practice task 2

---
+++ task-2.js
@@ -0,0 +1,24 @@
+/**
+ * ----------Practice Task-----------
+ * For this object below add a property named passenger capacity with value 5
+ * const car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020
+};
+
+ */
+
+const car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020
+}
+console.log(car);
+
+car.passengerCapacity = 5;
+
+console.log(car);
+
+
+
