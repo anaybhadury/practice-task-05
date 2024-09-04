6815b205e83c9fe97dca8474bcdf13e91ee478e7
anaybhadury <bhadury9292@gmail.com>
2024-09-04 21:34:09 +0600

    added practice task 4

---
+++ task-4.js
@@ -0,0 +1,18 @@
+/**
+ * ----------Practice Task-----------
+ * Count the number of properties.
+ * let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+ */
+
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+console.log(Object.keys(student).length);
